65f5915b637b38e43ccc70abd48bda4fac9443a5
--local <lassi.p.haapala@gmail.com>
2023-01-21 16:18:43 +0200

    improve-logging-and-add-profile-listing-feature

--- macrobuilder.py
+++ macrobuilder.py
@@ -20,7 +20,7 @@ if not FileLoader.exists("config.json"):
 
 # Start Macro listener with the parsed map
 def main(macro_map: dict):
-    logger.log("Main: Starting MacroBuilding");
+    logger.log("Main: Profile selected, starting MacroBuilding.");
 
     handler = HotkeyHandler(macro_map);
     handler.start();
@@ -34,6 +34,8 @@ def default():
 
     Note: requires one to be set first via the 'select' command
     """
+
+    logger.log("Main: Loading default profile.");
 
     profile = Config.get("default_profile");
 
@@ -62,6 +64,8 @@ def latest():
     Note: requires one to be set first via the 'select' command
     """
 
+    logger.log("Main: Loading latest profile.");
+
     profile = Config.get("latest_profile");
 
     if not profile:
@@ -73,7 +77,14 @@ def latest():
         logger.log("Main: No profile found for latest profile name: '" + profile + "'");
         exit();
 
-    set_default = typer.confirm("Do you want to set '" + profile + "' as your default profile?");
+    query = [
+        inquirer.Confirm(
+            "confirm",
+            message="Do you want to set '" + profile + "' as your default profile?"
+        ),
+    ];
+
+    set_default = inquirer.prompt(query)["confirm"];
     if set_default:
         Config.set("default_profile", profile);
         logger.log("Main: Updated default profile to: '" + profile + "'");
@@ -92,18 +103,58 @@ def select(
     Try to load a spesific profile defined with the PROFILE param.
     """
 
+    logger.log("Main: Selecting profile '" + profile + "'");
+
     profile_map = FileLoader.load(Config.get("profile_folder") + profile);
     if not profile_map:
         logger.log("Main: No profile found for given profile name: '" + profile + "'");
         exit();
 
-    set_default = typer.confirm("Do you want to set '" + profile + "' as your default profile?");
+    query = [
+        inquirer.Confirm(
+            "confirm",
+            message="Do you want to set '" + profile + "' as your default profile?"
+        ),
+    ];
+
+    set_default = inquirer.prompt(query)["confirm"];
     if set_default:
         Config.set("default_profile", profile);
         logger.log("Main: Updated default profile to: '" + profile + "'");
 
     Config.set("latest_profile", profile);
     logger.log("Main: Updated latest profile to: '" + profile + "'");
+
+    main(profile_map);
+    return;
+
+@init.command()
+def list():
+    """
+    Load all profiles for selection from the specified profile folder.
+    """
+
+    logger.log("Main: Enabling profile list selection.");
+
+    file_names = FileLoader.list(Config.get("profile_folder"));
+    if not file_names:
+        logger.log("Main: No profiles found in the specified profile folder.");
+        exit();
+
+    query = [
+        inquirer.List(
+            "profile",
+            message="Select the profile to load?",
+            choices=file_names,
+        ),
+    ];
+
+    selected_profile = inquirer.prompt(query)["profile"];
+
+    profile_map = FileLoader.load(Config.get("profile_folder") + selected_profile);
+    if not profile_map:
+        logger.log("Main: No profile found for given profile name: '" + selected_profile + "'");
+        exit();
 
     main(profile_map);
     return;
